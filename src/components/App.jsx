0e392d6dda3a38a561635198b8826163fe5a93ac
Rishab Munot <47574041+RishabMunot@users.noreply.github.com>
2020-05-06 18:49:16 +0530

    first commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  var [todos, setTodo] = useState(["Buy Milk"]);
+
+  var [state, setState] = useState("");
+
+  function handleChangeOfText(event) {
+    var value = event.target.value;
+    setState(value);
+  }
+
+  function addTodo() {
+    console.log(todos);
+
+    setTodo(prevValue => prevValue.push(state));
+  }
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
+        <input onChange={handleChangeOfText} value={state} type="text" />
+        <button onClick={addTodo}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item </li>
+          {todos.map(element => {
+            return <li>{element}</li>;
+          })}
         </ul>
       </div>
     </div>
